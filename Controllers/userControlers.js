d09d2b55915d7db457830e23be6b0db007c3b842
amirhossein-k <amirhossein.k787878@gmail.com>
2023-07-18 14:13:48 +0330

    dd

--- Controllers/userControlers.js
+++ Controllers/userControlers.js
@@ -65,17 +65,24 @@ const updateUser = asyncHandler(async(req,res)=>{
     
         var user = await User.findById(req.user._id)
         // const {name,family,phone_number,email,password} = req.body
-        
+        console.log(  req.body.address.address,'ggg')
         if(req.user){
             user.name = req.body.name || user.name;
-            user.detail =  {'family':req.body.family || user.detail[0].family ,"address": req.body.address||user.detail[0].address,"phone_number":req.body.phone_number || user.detail[0].phone_number, "history_buy": req.body.historyBuy ||user.detail[0].history_buy} || user.detail;
-            // user.family =  req.body.family || user.family;
+            const address = {
+            "location": req.body.address.location || user.detail[0].address.location,
+             "city":  req.body.address.city || user.detail[0].address.city,
+              "state":  req.body.address.state || user.detail[0].address.state,
+               "zipcode": req.body.address.zipcode || user.detail[0].address.zipcode 
+
+            }
+            user.detail =  {'family':req.body.family || user.detail[0].family ,"address": address,"phone_number":req.body.phone_number || user.detail[0].phone_number, "history_buy": req.body.historyBuy ||user.detail[0].history_buy} || user.detail;
+            
             user.email =  req.body.email || user.email;
             if (req.body.password) {
                 user.password = req.body.password;
             }
             const updatedUser = await user.save();
-            console.log(updatedUser,'famoy')
+            // console.log(updatedUser,'famoy')
             res.json({
                 _id: updatedUser._id,
                 name: updatedUser.name,
@@ -85,7 +92,8 @@ const updateUser = asyncHandler(async(req,res)=>{
                 isAdmin: updatedUser.isAdmin,
                 token: generateToken(updatedUser._id),
             });
-        }else{
+        }
+        else{
             res.status(404);
             throw new Error("User Not Found");
 
